d324d85f8662dc339c1bc6a76bd01a131ca720aa
Andrea Merello <andrea.merello@gmail.com>
2014-04-07 18:02:46 +0200

    merge linux indent from local with highlight from bitbucket

--- emacs.el
+++ emacs.el
@@ -1,6 +1,15 @@
 ; ------ disable vc-git
 (setq vc-handled-backends ())
 
+;; ------ plugins & extensions ---------
+(add-to-list 'load-path "~/emacs")
+(require 'xcscope)
+;; for col 80 mark
+(require 'column-marker)
+;; for current col hilight
+;;(require 'col-highlight)
+(require 'vline)
+(require 'highlight-chars)
 ;; ------ set all theme stuff ---------
 (require 'xcscope)
 (require 'color-theme)
@@ -46,41 +55,100 @@
 
 ;; -------  indentation and tabs for C code
 
-(setq c-default-style "linux")
+
+; default to kernel style
+(setq c-default-style "linux-tabs-only")
+(setq indent-tabs-mode t)
 ;(setq c-basic-indent 2)
 ;(setq tab-width 2)
 ;(setq indent-tabs-mode nil)
 
-;; ------ tabs hilight stuff  ------------
-(setq highlight-tabs 1)
-(setq highlight-trailing-whitespace 1)
+;; ------ face for all warnings (trailing whitespace, > 80 cols.. 
+(defface my-warning-face '((t (:background "#900000")))
+  "Face used for a column marker.  Usually a background color."
+  :group 'faces)
 
+;; ------ tabs & other special chars hilight stuff  ------------
+;(setq highlight-tabs 1)
+;(setq highlight-trailing-whitespace 1)
+;(setq trailing-whitespace 'my-warning-face)
+(defface hc-trailing-whitespace '((t (:inherit 'my-warning-face)))
+  "Face used for a column marker.  Usually a background color."
+  :group 'Highlight-Characters :group 'faces)
+
+(defface hc-tab '((t (:background "#505000")))
+  "*Face for highlighting tab characters (`C-i') in Font-Lock mode."
+  :group 'Highlight-Characters :group 'faces)
+
+
+;; ------- current col hilight -----------
+(setq vline-use-timer t)
+(setq vline-idle-time 0.4)
+
+;;(setq vline-style 'compose) ;; compose brokes vertial font size
+(defface my-vline-face
+  '((t (:background "#151515")))
+  "A default face for vertical line highlighting in visual lines."
+  :group 'vline)
+
+;; don't touch background here
+(defface my-vline-visual-face
+  '((t ()))
+  "A default face for vertical line highlighting in visual lines."
+  :group 'vline)
+
+;; wrapped rows are draw differently
+(setq vline-face 'my-vline-face)
+;; face for wrapped rows
+(setq vline-visual-face 'my-vline-visual-face)
+
+;; ------ set face for 80 col highligh -------------------------
+(setq column-marker-1-face 'my-warning-face)
 
 ;; ------- when searching TAGS in code, make it case SENSITIVE
 (setq tags-case-fold-search nil)
 
 ;; ------ set default font
-(when window-system  
+(when window-system
     (set-face-attribute 'default nil :font "Monospace-10")
 )
+
+(defun c-lineup-arglist-tabs-only (ignored)
+  "Line up argument lists by tabs, not spaces"
+  (let* ((anchor (c-langelem-pos c-syntactic-element))
+	  (column (c-langelem-2nd-pos c-syntactic-element))
+	   (offset (- (1+ column) anchor))
+	    (steps (floor offset c-basic-offset)))
+    (* (max steps 1)
+       c-basic-offset)))
+
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            ;; Add kernel style
+            (c-add-style
+             "linux-tabs-only"
+             '("linux" (c-offsets-alist
+                        (arglist-cont-nonempty
+                         c-lineup-gcc-asm-reg
+                         c-lineup-arglist-tabs-only))))))
 
 
 ; -------------------- custom funcs --------------
 
 (defun my-indent-linux ()
-(interactive)
-(setq c-default-style "linux")
-
-
+  (interactive)
+  (setq indent-tabs-mode t)
+  (c-set-style "linux-tabs-only")
 )
 
+
 (defun my-indent-2 ()
-(interactive)
-;;(setq c-basic-indent 2)
-(setq c-basic-offset 2)
-(setq tab-width 2)
-(setq indent-tabs-mode nil)
-;(setq indent-line-function 'insert-tab)
+  (interactive)
+  ;;(setq c-basic-indent 2)
+  (setq c-basic-offset 2)
+  (setq tab-width 2)
+  (setq indent-tabs-mode nil)
+					;(setq indent-line-function 'insert-tab)
 )
 
 ; ------- GTAGS RELATED 
@@ -152,26 +220,31 @@
   (message "gdb for ARM/openocd")
 )
 
+;(defun my-show-trailing-whitespace ()
+;(setq show-trailing-whitespace t))
 
 (defun my-show-tabs ()
   (interactive)
-  (let ((i 0) (disptab make-display-table)))
-    (while (&lt; i 32)
-      (or (= i ?\n)
-          (aset disptab i (vector ?^ (+ i 64))))
-      (setq i (1+ i)))
-    (aset disptab 127 (vector ?^ ??))
-    (setq buffer-display-table disptab)))
+ ; (standard-display-ascii ?\t "^I"))
+  (hc-toggle-highlight-tabs))
+;  (let ((i 0) (disptab make-display-table)))
+;    (while (&lt; i 32)
+;      (or (= i ?\n)
+;           (aset disptab i (vector ?^ (+ i 64))))
+;      (setq i (1+ i)))
+;    (aset disptab 127 (vector ?^ ??))
+;    (setq buffer-display-table disptab)))
 
-(defun my-hide-tabs ()
-  (interactive)
-  (let ((i 0) (disptab make-display-table)))
-    (while (&lt; i 32)
-      (or (= i ?\n) (= i ?\t)
-          (aset disptab i (vector ?^ (+ i 64))))
-      (setq i (1+ i)))
-    (aset disptab 127 (vector ?^ ??))
-    (setq buffer-display-table disptab)))
+;(defun my-hide-tabs ()
+;  (interactive)
+;  (hc-highlight-tabs nil))
+;  (let ((i 0) (disptab make-display-table)))
+;    (while (&lt; i 32)
+;      (or (= i ?\n) (= i ?\t)
+;          (aset disptab i (vector ?^ (+ i 64))))
+;      (setq i (1+ i)))
+;    (aset disptab 127 (vector ?^ ??))
+;    (setq buffer-display-table disptab)))
 ;; end tabs hilight stuff  ------------
 
 
@@ -181,8 +254,13 @@
 ;;(add-hook 'after-load-hook 'my-gtags-load-hook)
 ;;(add-hook 'c-mode-common-hook 'my-gtags-mode)
 
-
-
+;; --------- hilight column #80 -------------------------
+(add-hook 'prog-mode-hook (lambda() (interactive) (column-marker-1 80)))
+;; --------- enables current vertical colum highlight
+(add-hook 'prog-mode-hook 'vline-mode)
+;; --------- highlight trailing spaces
+;(add-hook 'prog-mode-hook 'my-show-trailing-whitespace)
+(add-hook 'prog-mode-hook 'hc-highlight-trailing-whitespace)
 
 ; ---------------------- key bindings ---------------------
 (global-set-key [(meta down)] 'scroll-up-line)
