215ee03d9ddedd716d1886fd42a3e0be6be26285
Andrea Merello <andrea.merello@gmail.com>
2014-06-23 10:56:24 +0200

    default auto fill to 80 cols

--- conf/col80.el
+++ conf/col80.el
@@ -1,7 +1,7 @@
 ;; for col 80 mark
 (require 'column-marker)
 
-;; ------ face for all warnings (trailing whitespace, > 80 cols.. 
+;; ------ face for all warnings (trailing whitespace, > 80 cols..
 (defface my-warning-80face '((t (:background "#900000")))
   "Face used for a column marker.  Usually a background color."
   :group 'faces)
@@ -9,3 +9,6 @@
 (setq column-marker-1-face 'my-warning-80face)
 
 (add-hook 'prog-mode-hook (lambda() (interactive) (column-marker-1 80)))
+
+;; -- default for text justify
+(setq-default fill-column 80)
