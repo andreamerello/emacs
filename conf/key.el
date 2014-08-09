8c1394d162ac699c177a175a2b18d288f6bd2cfe
Andrea Merello <andrea.merello@gmail.com>
2014-08-09 02:30:10 +0200

    added key for windows enlarge/shrink

--- conf/key.el
+++ conf/key.el
@@ -3,5 +3,10 @@
 (global-set-key [(meta up)] 'scroll-down-line)
 (global-set-key [(ctrl .)] 'gtags-find-rtag)
 
+(global-set-key [(control meta up)] 'enlarge-window)
+(global-set-key [(control meta down)] 'shrink-window)
+(global-set-key [(control meta right)] 'enlarge-window-horizontally)
+(global-set-key [(control meta left)] 'shrink-window-horizontally)
+
 ;; TODO find a good key
 ;;(global-set-key [f8] 'gtags-show-callers)
