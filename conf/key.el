761163bc1e3cfef865105f6934f6b58af353592c
Andrea Merello <andrea.merello@gmail.com>
2016-11-18 09:33:07 +0100

    put my key bindings in a minor mode
    .. the net effect is that they take precedence over keybindings specific of
    other minor modes..

--- conf/key.el
+++ conf/key.el
@@ -1,16 +1,45 @@
-; ---------------------- key bindings ---------------------
-(global-set-key [(meta down)] 'scroll-up-line)
-(global-set-key [(meta up)] 'scroll-down-line)
+;;---------------------- key bindings ---------------------
 
-(global-set-key [(control meta up)] 'enlarge-window)
-(global-set-key [(control meta down)] 'shrink-window)
-(global-set-key [(control meta right)] 'enlarge-window-horizontally)
-(global-set-key [(control meta left)] 'shrink-window-horizontally)
+;; add minor mode insted of using directly things like
+;; (global-set-key "\C-l\C-l" 'redraw-display)
+;; so that
+;; 1 - key bindings can be enabled/disabled in one shot by enabling/disabling
+;;     the minor mode
+;; 2 - key bindings take precedence over key bindings in other minor mode
+;;     (es C-o in compiler ouput buffer)
+;;
+;; http://stackoverflow.com/questions/683425/globally-override-key-binding-in-emacs
 
-(global-set-key (kbd "C-o") 'other-window)
+(defvar my-keys-minor-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map [(meta down)] 'scroll-up-line)
+    (define-key map [(meta up)] 'scroll-down-line)
 
-(global-set-key (kbd "M-n") 'my-next-error)
-(global-set-key (kbd "M-p") 'my-previous-error)
-(global-set-key (kbd "C-c k") 'my-kill-compiler-output)
+    (define-key map [(control meta up)] 'enlarge-window)
+    (define-key map [(control meta down)] 'shrink-window)
+    (define-key map [(control meta right)] 'enlarge-window-horizontally)
+    (define-key map [(control meta left)] 'shrink-window-horizontally)
+
+    (define-key map (kbd "C-o") 'other-window)
+
+    (define-key map (kbd "M-n") 'my-next-error)
+    (define-key map (kbd "M-p") 'my-previous-error)
+    (define-key map (kbd "C-c k") 'my-kill-compiler-output)
 ;; TODO find a good key
-;;(global-set-key [f8] 'gtags-show-callers)
+;;(define-key map [f8] 'gtags-show-callers)
+    map)
+  "my-keys-minor-mode keymap.")
+
+(define-minor-mode my-keys-minor-mode
+  "A minor mode so that my key settings override annoying major modes."
+  :init-value t
+  :lighter " my-keys")
+
+(my-keys-minor-mode 1)
+
+;; disable my custom key bindings in the minibuffer
+;;
+(defun my-minibuffer-setup-hook ()
+  (my-keys-minor-mode 0))
+
+(add-hook 'minibuffer-setup-hook 'my-minibuffer-setup-hook)
